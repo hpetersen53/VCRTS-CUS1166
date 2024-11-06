f72d313402fd435b03415e1357031ae43fa2eee1
hpetersen <hannpetersen53@gmail.com>
2024-11-05 22:17:16 -0500

    Merge branch 'master' of https://github.com/hpetersen53/VCRTS-CUS1166

--- src/main/Job.java
+++ src/main/Job.java
@@ -3,35 +3,36 @@ package main;
 import java.time.LocalDate;
 
 public class Job {
-    private int clientID;
-    private int levelOfRedundancy;
-    private int jobDuration;
-    private double payout;
-    private String title;
-    private LocalDate deadline;
-    private String attachedFileName;
+	private int clientID;
+	private int levelOfRedundancy;
+	private int jobDuration;
+	private double payout;
+	private String title;
+	private LocalDate deadline;
+	private String attachedFileName;
 
-    public Job(int clientID, int levelOfRedundancy, int jobDuration, double payout, String title, LocalDate deadline, String attachedFileName) {
-        this.clientID = clientID;
-        this.levelOfRedundancy = levelOfRedundancy;
-        this.jobDuration = jobDuration;
-        this.payout = payout;
-        this.title = title;
-        this.deadline = deadline;
-        this.attachedFileName = attachedFileName;
-    }
+	public Job(int clientID, int levelOfRedundancy, int jobDuration, double payout, String title, LocalDate deadline,
+			String attachedFileName) {
+		this.clientID = clientID;
+		this.levelOfRedundancy = levelOfRedundancy;
+		this.jobDuration = jobDuration;
+		this.payout = payout;
+		this.title = title;
+		this.deadline = deadline;
+		this.attachedFileName = attachedFileName;
+	}
 
-    public String getDetails() {
-        return "Job Title: " + title + "\nPayout: " + payout + "\nDeadline: " + deadline + "\nFile: " + (attachedFileName == null ? "None" : attachedFileName);
-    }
+	public String getDetails() {
+		return "Job Title: " + title + "\nPayout: " + payout + "\nDeadline: " + deadline + "\nFile: "
+				+ (attachedFileName == null ? "None" : attachedFileName);
+	}
 
-    public String toFileString() {
-        return clientID + "," + title + "," + payout + "," + deadline + "," + (attachedFileName == null ? "" : attachedFileName);
-    }
-    
-    public int getID() {
-    	return clientID;
-    }
-}
+	public String toFileString() {
+		return clientID + "," + title + "," + payout + "," + deadline + ","
+				+ (attachedFileName == null ? "" : attachedFileName);
+	}
 
+	public int getID() {
+		return clientID;
+	}
 }
