815556b5b4a789475d0d0d844f366a25fdb1cf77
Ruszeii <123785723+Ruszeii@users.noreply.github.com>
2024-11-12 18:53:14 -0500

    Update GUIWindow.java

--- src/gui/GUIWindow.java
+++ src/gui/GUIWindow.java
@@ -3,9 +3,10 @@ package gui;
 import java.awt.Color;
 import java.awt.Font;
 import java.awt.GridBagConstraints;
-import java.awt.GridLayout;
+
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.awt.Insets;
 
 import javax.swing.JButton;
 import javax.swing.JFrame;
@@ -49,8 +50,12 @@ public class GUIWindow {
         });
         
 
-		JLabel welcome = new JLabel("Welcome");
-		welcome.setHorizontalAlignment(JLabel.CENTER);
+		JLabel welcome = new JLabel("Welcome to the VCRTS!");
+		welcome.setFont(new Font("Verdana", Font.BOLD, 15));
+		
+		JLabel introduction = new JLabel("The Vehicular Cloud Real Time System was made for managing and organizing computation resources and jobs in vehicular cloud.");
+introduction.setFont(new Font("Verdana", Font.PLAIN,12));
+
 		JPanel jPanel = new JPanel(new GridBagLayout());
 		GridBagConstraints gbc = new GridBagConstraints();
 		gbc.insets = new java.awt.Insets(10,10,10,10);
@@ -59,29 +64,46 @@ public class GUIWindow {
 		gbc.gridy = 0;
 		gbc.gridwidth = 2;
 		gbc.anchor = GridBagConstraints.CENTER;
+		gbc.weightx = 1.0;
+		gbc.weighty = 0.1;
 		jPanel.add(welcome, gbc);
-		
+
 		gbc.gridx = 0;
 		gbc.gridy = 1;
-		gbc.gridwidth = 1;
+		gbc.gridwidth = 2;
 		gbc.anchor = GridBagConstraints.CENTER;
-		jPanel.add(user1Btn, gbc);
+		gbc.weightx = 1.0;
+		gbc.weighty = 0.2;
+		jPanel.add(introduction, gbc);
+		
+		gbc.gridx = 0;
+        gbc.gridy = 2; 
+        gbc.gridwidth = 1;
+        gbc.anchor = GridBagConstraints.EAST;
+		gbc.weightx = 0.5;
+        gbc.insets = new java.awt.Insets(10, 10, 10, 5); 
+        jPanel.add(user1Btn, gbc);
 		
 		gbc.gridx = 1;
-		gbc.gridy = 1;
-		jPanel.add(loginBtn, gbc);
+        gbc.gridy = 2; 
+        gbc.anchor = GridBagConstraints.WEST;
+		gbc.weightx = 0.5;
+        gbc.insets = new java.awt.Insets(10, 5, 10, 10); 
+        jPanel.add(loginBtn, gbc);
 		
 		
 		jFrame = new JFrame();
 
 		jFrame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE); 
 		jFrame.setTitle("Vehicular Cloud Real-Time System"); 
-		jFrame.setResizable(true); 
 		jFrame.setVisible(true); 
 		jFrame.setSize(400, 500);
-		//jFrame.setLayout(null);
-		
 		jFrame.add(jPanel);
+		jFrame.pack();
+		jFrame.setLocationRelativeTo(null);
+		
+		
+		
 
 		
 		
